38f41bedb3664fb8c525ceffc0a90795b6ac1060
HeshanChamika <it22194930@my.sliit.lk>
2026-04-10 22:05:06 +0530

    feat: update default data by removing and adding attendees and items

--- frontend/src/data/defaultData.js
+++ frontend/src/data/defaultData.js
@@ -6,7 +6,6 @@ export const defaultItems = [
   { name: "Charcoal", unitPrice: 500, quantity: 2, isAlcoholic: false },
   { name: "Beverages & Snacks", unitPrice: 3500, quantity: 1, isAlcoholic: false },
   { name: "Rice or Kottu", unitPrice: 1000, quantity: 6, isAlcoholic: false },
-  { name: "Corn", unitPrice: 100, quantity: 10, isAlcoholic: false },
 
   // Alcoholic items
   // { name: "Whisky", unitPrice: 12000, quantity: 1, isAlcoholic: true },
@@ -17,17 +16,15 @@ export const defaultPeople = [
   { name: "Heshan", isAlcoholic: false },
   { name: "Chamiru", isAlcoholic: false },
   { name: "Amantha", isAlcoholic: false },
-  { name: "Akindu", isAlcoholic: false },
   { name: "Sasanka", isAlcoholic: false },
   { name: "Manuja", isAlcoholic: false },
-  { name: "Nadil", isAlcoholic: false },
 
   // Alcoholic people
   { name: "Venusha", isAlcoholic: true },
   { name: "Manuka", isAlcoholic: true },
   { name: "Datha", isAlcoholic: true },
-  { name: "Susira", isAlcoholic: true },
   { name: "Sanjula", isAlcoholic: true },
   { name: "Chameen", isAlcoholic: true },
   { name: "Gadda", isAlcoholic: true },
+  { name: "Nadil", isAlcoholic: true },
 ];
